3aaeed3e007c1e2ad4cf0663c22ef2021335376d
Marco Gulino <marco@gulinux.net>
2013-03-04 17:10:21 +0100

    Software authentication

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "streamingapp.h"
+#include "session.h"
 #include <Wt/WServer>
 
 
@@ -10,6 +11,23 @@ Wt::WApplication *createApplication(const Wt::WEnvironment& env)
 }
 
 
-int main(int argc, char **argv) {
-  return Wt::WRun(argc, argv, createApplication);
+int main(int argc, char **argv)
+{
+  try {
+    Wt::WServer server(argv[0]);
+
+    server.setServerConfiguration(argc, argv, WTHTTP_CONFIGURATION);
+    server.addEntryPoint(Wt::Application, createApplication);
+
+    Session::configureAuth();
+
+    if (server.start()) {
+      Wt::WServer::waitForShutdown();
+      server.stop();
+    }
+  } catch (Wt::WServer::Exception& e) {
+    std::cerr << e.what() << std::endl;
+  } catch (std::exception &e) {
+    std::cerr << "exception: " << e.what() << std::endl;
+  }
 }
