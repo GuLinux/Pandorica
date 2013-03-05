3dfe4e3d2d53c5a0061884c01a3d87291aa80b39
Marco Gulino <marco@gulinux.net>
2013-03-05 01:37:30 +0100

    Allineamento lista dettagli

--- sessiondetails.h
+++ sessiondetails.h
@@ -6,16 +6,20 @@
 #include <Wt/Dbo/ptr>
 #include <Wt/WGlobal>
 #include <Wt/WDateTime>
+#include <boost/filesystem/path.hpp>
 #include "authorizeduser.h"
 
 class SessionInfo;
 class SessionDetails {
 public:
   SessionDetails() {}
+  SessionDetails(boost::filesystem::path path)
+    : _filename(path.filename().string()), _filepath(path.string()), _playStarted(Wt::WDateTime::currentDateTime().toTime_t()) {}
   ~SessionDetails() {}
   
   std::string filename() const { return _filename; }
   std::string filepath() const { return _filepath; }
+  void ended() { if(!_playEnded) _playEnded = Wt::WDateTime::currentDateTime().toTime_t(); }
   Wt::WDateTime playStarted() const { return Wt::WDateTime::fromTime_t(_playStarted); }
   Wt::WDateTime playEnded() const { return Wt::WDateTime::fromTime_t(_playEnded); }
   
