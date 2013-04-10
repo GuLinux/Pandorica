7f05fd252372c9c1ff08b01f16800c87ee7fd45c
Marco Gulino <marco@gulinux.net>
2013-04-10 17:45:45 +0200

    Aggiunta prima bozza edit path gruppo

--- group.h
+++ group.h
@@ -6,7 +6,21 @@
 #include <Wt/WGlobal>
 
 class User;
+class Group;
 namespace dbo = Wt::Dbo;
+
+class GroupPath {
+public:
+  template<class Action>
+  void persist(Action& a) {
+    dbo::field(a, _path, "path");
+    dbo::belongsTo(a, _group, "group");
+  }
+  inline std::string path() const { return _path; }
+private:
+  std::string _path;
+  dbo::ptr<Group> _group;
+};
 
 class Group
 {
@@ -18,13 +32,14 @@ public:
     dbo::hasMany(a, users, dbo::ManyToMany, "groups_users");
     dbo::field(a, _groupName, "group_name");
     dbo::field(a, _isAdmin, "is_admin");
+    dbo::hasMany(a, groupPaths, dbo::ManyToOne, "group");
   }
   inline std::string groupName() const { return _groupName; }
   inline void setGroupName(const std::string &groupName) { _groupName = groupName; }
   inline bool isAdmin() const { return _isAdmin; }
   
   dbo::collection< dbo::ptr<User> > users;
-  
+  dbo::collection< dbo::ptr<GroupPath> > groupPaths;
 private:
   std::string _groupName;
   bool _isAdmin;
