c1df92155ee841df6322ed0173aa8d852d3001e2
Marco Gulino <marco@gulinux.net>
2013-05-17 21:16:21 +0200

    Utilizzo di recursive_directory_iterator

--- mediacollection.cpp
+++ mediacollection.cpp
@@ -15,17 +15,17 @@ using namespace StreamingPrivate;
 namespace fs = boost::filesystem;
 
 MediaCollection::MediaCollection(string basePath, Session* session, WApplication* parent)
-  : WObject(parent), d(new MediaCollectionPrivate(basePath, session, parent))
+    : WObject(parent), d(new MediaCollectionPrivate(basePath, session, parent))
 {
 }
 
 void MediaCollection::rescan()
 {
-  Dbo::Transaction t(*d->session);
-  d->allowedPaths = d->session->user()->allowedPaths();
-  d->collection.clear();
-  d->listDirectory(d->basePath);
-  // TODO: muovere su nuovo thread, al momento pare non andare (eccezione postgres)
+    Dbo::Transaction t(*d->session);
+    d->allowedPaths = d->session->user()->allowedPaths();
+    d->collection.clear();
+    d->listDirectory(d->basePath);
+    // TODO: muovere su nuovo thread, al momento pare non andare (eccezione postgres)
 //   WServer::instance()->post(d->app->sessionId(), [=] {
     d->scanned.emit();
 //     wApp->triggerUpdate();
@@ -34,58 +34,54 @@ void MediaCollection::rescan()
 
 bool MediaCollectionPrivate::isAllowed(filesystem::path path)
 {
-  for(string p: allowedPaths) {
-    if(path.string().find(p) != string::npos)
-      return true;
-  }
-  return false;
+    for(string p: allowedPaths) {
+        if(path.string().find(p) != string::npos)
+            return true;
+    }
+    return false;
 }
 
 
 void MediaCollectionPrivate::listDirectory(filesystem::path path)
 {
-  vector<fs::path> v;
-  copy(fs::directory_iterator(path), fs::directory_iterator(), back_inserter(v));
-  sort(v.begin(), v.end());
-  for(fs::path path: v) {
+    vector<fs::directory_entry> v;
     try {
-      if(fs::is_directory(path)) {
-        listDirectory(path);
-      }
-      else {
-        Media media{path};
-        if(media.mimetype() != "UNSUPPORTED" && isAllowed(path)) {
-          collection[media.uid()] = media;
+      copy(fs::recursive_directory_iterator(path, fs::symlink_option::recurse), fs::recursive_directory_iterator(), back_inserter(v));
+        sort(v.begin(), v.end());
+        for(fs::directory_entry entry: v) {
+          fs::path path = entry.path();
+          Media media {path};
+          if(fs::is_regular_file(path) && media.mimetype() != "UNSUPPORTED" && isAllowed(path)) {
+            collection[media.uid()] = media;
+          }
         }
-      }
     } catch(std::exception &e) {
-      log("error") << "Error trying to add path " << path << ": " << e.what();
+        log("error") << "Error trying to add path " << path << ": " << e.what();
     }
-  }
 }
 
 map< string, Media > MediaCollection::collection() const
 {
-  return d->collection;
+    return d->collection;
 }
 
 Media MediaCollection::media(string uid) const
 {
-  return d->collection[uid];
+    return d->collection[uid];
 }
 
 Signal<>& MediaCollection::scanned()
 {
-  return d->scanned;
+    return d->scanned;
 }
 
 
 MediaCollection::~MediaCollection()
 {
-  delete d;
+    delete d;
 }
 
 filesystem::path MediaCollection::rootPath() const
 {
-  return d->basePath;
+    return d->basePath;
 }
