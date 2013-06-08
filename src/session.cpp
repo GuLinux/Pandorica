5ce7189c8c68c67504457c2345814545abbe84b3
Marco Gulino <marco.gulino@gmail.com>
2013-06-08 19:43:06 +0200

    Fix License

--- src/session.cpp
+++ src/session.cpp
@@ -1,8 +1,23 @@
-/*
- * Copyright (C) 2008 Emweb bvba, Kessel-Lo, Belgium.
- *
- * See the LICENSE file for terms of use.
- */
+/***********************************************************************
+Copyright (c) 2013 "Marco Gulino <marco.gulino@gmail.com>"
+
+This file is part of Pandorica: https://github.com/GuLinux/Pandorica
+
+Pandorica is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details (included the COPYING file).
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+***********************************************************************/
+
+
 
 #include "session.h"
 
