280a2ae5706df9395c50d5869bee3a8ed19ad75d
Marco Gulino <marco@gulinux.net>
2013-06-10 20:48:27 +0200

    fix (?) for android

--- src/player/html5player.cpp
+++ src/player/html5player.cpp
@@ -165,7 +165,7 @@ void HTML5Player::playerReady()
 {
   
   map<string,string> mediaElementOptions = {
-    {"AndroidUseNativeControls", "true"}
+    {"AndroidUseNativeControls", "false"}
   };
   
   // works in theory, but it goes with double subs on chrome 
