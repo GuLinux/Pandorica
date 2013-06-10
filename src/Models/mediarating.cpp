99e8820834849b9201e5ee9b942bb1d2bf6a8019
Marco Gulino <marco.gulino@xpeppers.com>
2013-06-10 10:07:25 +0200

    Warning--

--- src/Models/mediarating.cpp
+++ src/Models/mediarating.cpp
@@ -28,5 +28,5 @@ Ratings MediaRating::ratingFor(const Media& media, Wt::Dbo::Transaction& transac
   long ratingsCount = transaction.session().query<long>("select count(id) from media_rating WHERE media_id = ?").bind(media.uid());
   if(ratingsCount == 0) return {0, -1};
   double ratings = transaction.session().query<double>("select sum(rating) from media_rating WHERE media_id = ?").bind(media.uid()).resultValue();
-  return {ratingsCount, boost::math::round(ratings / (double) ratingsCount)};
+  return {ratingsCount, (int) (boost::math::round(ratings / (double) ratingsCount)) };
 }
