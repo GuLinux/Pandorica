ffeaac0e0808699aeef3de14e67e2ebf7361fb5c
Marco Gulino <marco@gulinux.net>
2013-06-24 13:27:37 +0200

    compact code

--- src/pandorica.cpp
+++ src/pandorica.cpp
@@ -93,7 +93,7 @@ P::PandoricaPrivate::PandoricaPrivate(Pandorica *q) : q(q), playSignal(q, "playS
 Pandorica::Pandorica( const Wt::WEnvironment& environment) : WApplication(environment), d(new P::PandoricaPrivate(this)) {
 //   useStyleSheet( wApp->resourcesUrl() + "filesystemorm.css");
   useStyleSheet(Settings::staticPath("/Pandorica.css"));
-  addMetaLink(Settings::staticPath("/icons/favicon.png"), "shortcut icon", string{}, string{}, string{}, string{}, false);
+  addMetaLink(Settings::staticPath("/icons/favicon.png"), "shortcut icon", {}, {}, {}, {}, false);
   requireJQuery(Settings::staticPath("/jquery.min.js"));
   require(Settings::staticPath("/bootstrap/js/bootstrap.min.js"));
   
