a8f33cf82b4ee7474a5219347cfc6e5d492be2e2
Marco Gulino <marco@gulinux.net>
2013-09-08 12:09:20 +0200

    Better script for migrations creation

--- src/migrations/migrations.h
+++ src/migrations/migrations.h
@@ -2,11 +2,13 @@
 #define MIGRATIONS_H
 #include "Wt-Commons/migratedbo.h"
 #include "migrations/create_migrations_table.h"
+// migrations_includes_placeholder
 namespace Migrations
 {
   WtCommons::Migrations migrations
   {
     std::shared_ptr<WtCommons::DboMigration>(createMigrationTable()),
+// migrations_init_placeholder
   };
 }
 #endif
