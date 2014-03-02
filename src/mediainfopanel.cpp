e4bfc64e8d40d73d8e5298cdfa2e65770324f892
Marco Gulino <marco@gulinux.net>
2014-03-02 14:21:50 +0100

    Fix infopanel cover size

--- src/mediainfopanel.cpp
+++ src/mediainfopanel.cpp
@@ -103,7 +103,7 @@ void MediaInfoPanel::info( Media &media )
     if( fullImageAttachment )
       fullImage = fullImageAttachment->link( fullImageAttachment, t, header );
 
-    WAnchor *fullImageLink = new WAnchor {fullImage, new WImage{previewLink, title}};
+    WAnchor *fullImageLink = new WAnchor {fullImage, WW<WImage>(previewLink, title).css("infopanel-cover")};
     fullImageLink->setTarget( Wt::AnchorTarget::TargetNewWindow );
     header->addWidget( fullImageLink );
   }
