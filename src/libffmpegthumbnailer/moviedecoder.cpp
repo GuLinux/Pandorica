43be07741c761b3237f2059be3aabe7cbd4f545a
Marco Gulino <marco@gulinux.net>
2015-03-23 19:27:41 +0100

    lock all initialize method

--- src/libffmpegthumbnailer/moviedecoder.cpp
+++ src/libffmpegthumbnailer/moviedecoder.cpp
@@ -63,19 +63,17 @@ void MovieDecoder::initialize(const string& filename)
     av_register_all();
     avcodec_register_all();
 
+    auto lock = ThreadPool::lock("ffmpeg_avcodec_open");
     string inputFile = filename == "-" ? "pipe:" : filename;
     m_AllowSeek = (filename != "-") && (filename.find("rtsp://") != 0);
-    {
-      auto lock = ThreadPool::lock("ffmpeg_avcodec_open");
 #if LIBAVCODEC_VERSION_MAJOR < 53
-      if ((!m_FormatContextWasGiven) && av_open_input_file(&m_pFormatContext, inputFile.c_str(), NULL, 0, NULL) != 0)
+    if ((!m_FormatContextWasGiven) && av_open_input_file(&m_pFormatContext, inputFile.c_str(), NULL, 0, NULL) != 0)
 #else
-          if ((!m_FormatContextWasGiven) && avformat_open_input(&m_pFormatContext, inputFile.c_str(), NULL, NULL) != 0)
+        if ((!m_FormatContextWasGiven) && avformat_open_input(&m_pFormatContext, inputFile.c_str(), NULL, NULL) != 0)
 #endif
-      {
-                  destroy();
-          throw logic_error(string("Could not open input file: ") + filename);
-      }
+    {
+                destroy();
+        throw logic_error(string("Could not open input file: ") + filename);
     }
 #if LIBAVCODEC_VERSION_MAJOR < 53
     if (av_find_stream_info(m_pFormatContext) < 0)
