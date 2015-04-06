b14b90beeb123ddd57adedf62a045b79acc6e626
Marco Gulino <marco@gulinux.net>
2015-04-06 12:12:47 +0200

    using internal submodule for ffmpeg to improve build compatibility

--- cmake/FindFFMPEG.cmake
+++
@@ -1,7 +0,0 @@
-include(FindPkgConfig)
-# TODO : difficult to find on windows 32. Remove,  or implement alternatives (standalone ffmpeg binary? )
-pkg_check_modules(AVUTIL libavutil REQUIRED)
-pkg_check_modules(AVFORMAT libavformat REQUIRED)
-pkg_check_modules(AVCODEC libavcodec REQUIRED)
-pkg_check_modules(SWSCALE libswscale REQUIRED)
-pkg_check_modules(LIBPNG libpng REQUIRED)
